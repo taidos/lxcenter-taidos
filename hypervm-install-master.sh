8eb42018828c1f68f45ddcd73fb8a474270ba37e
Carlos Faustino <70132833+taidos@users.noreply.github.com>
2020-09-15 10:36:41 +0100

    HyperVM repaired Install
    Im working arround this file like the pool i made with the program-install.zip needed. with the repo redirected to our's and the main source files to try to install are allmost here.
    
    Any help will be great.
    
    Thanks for me, Rackhost and ifastnet

---
+++ hypervm-install-master.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+if ! [ -f /usr/bin/yum ] ; then
+      echo You at least need yum installed for this to work...
+	  echo Please contact our support personnel Or visit the forum at http://github.com/taidos
+	  echo "                                "
+	  exit
+fi
+
+if [ -f /usr/bin/yum ] ; then
+	yum -y install php wget zip unzip 
+else 
+	up2date --nox --nosig php wget zip unzip
+fi
+
+if ! [ -f /usr/bin/php ] ; then
+	echo installing php failed. Please fix yum/up2date.
+	exit
+fi
+
+rm -f program-install.zip
+wget https://github.com/taidos/lxcenter-repo/raw/076478cec3bacb4af4f9ebc5fbe60310a842aea6/program-install.zip
+
+export PATH=/usr/sbin:/sbin:$PATH
+unzip -oq program-install.zip
+cd program-install/hypervm-linux
+php ~/program-install/lxins.php --install-type=master $* | tee hypervm_install.log
+
+
+
+
