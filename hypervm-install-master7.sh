ed98991ea077139da65df97a53f2540a3598c4d3
Carlos Faustino <70132833+taidos@users.noreply.github.com>
2021-01-23 10:48:15 +0000

    Update and rename hypervm-install-master.sh to hypervm-install-master7.sh
    Installation only for CentOS7

--- hypervm-install-master7.sh
+++ hypervm-install-master7.sh
@@ -23,6 +23,7 @@ wget https://github.com/taidos/lxcenter-repo/raw/master/program-install.zip
 rpm -Uvh https://download-ib01.fedoraproject.org/pub/epel/7/x86_64/Packages/l/libc-client-2007f-16.el7.x86_64.rpm
 rpm -Uvh https://download-ib01.fedoraproject.org/pub/epel/7/x86_64/Packages/l/libdb4-4.8.30-13.el7.x86_64.rpm
 rpm -Uvh https://download-ib01.fedoraproject.org/pub/epel/7/x86_64/Packages/l/libmcrypt-2.5.8-13.el7.x86_64.rpm
+rpm -Uvh https://github.com/taidos/lxcenter-taidos/raw/master/hypervm-core-lighttpd-1.4.54-1.el7.x86_64.rpm
 
 export PATH=/usr/sbin:/sbin:$PATH
 unzip -oq program-install.zip
